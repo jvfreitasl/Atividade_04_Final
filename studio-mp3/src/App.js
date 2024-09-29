15a574ecf098c24c15f8e94c19f7f0e4e2a2badf
João Victor de F. Lima <jvdfreitas@gmail.com>
2024-09-28 22:55:50 -0300

    Build `Header` and another components

--- studio-mp3/src/App.js
+++ studio-mp3/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
       </header>
+      <body className='App-body'>
+      </body>
+      <footer className='App-footer'>
+      </footer>
     </div>
   );
 }
---
+++ studio-mp3/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='containerHeader'>
+      <h1>Studio MP3</h1>
+      <h2>
+        <em>Seja bem vindo ao palco mp3 das suas músicas favoritas.</em>
+      </h2>
+    </div>
+  );
+};
+
+export default Header;
